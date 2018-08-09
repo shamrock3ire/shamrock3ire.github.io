7c6387ec5f8d95d7e2abbc5da069ef9cff44ee13
shamrock3ire <shamrock3ire@gmail.com>
2018-08-10 06:05:54 +0900

    2018年9月入所分空き状況

--- js/papamamap.js
+++ js/papamamap.js
@@ -414,7 +414,7 @@ Papamamap.prototype.getPopupContent = function(feature)
         content += '<th>欠員</th>';
         content += '<td>';
         if (feature.get('v_age') !== null) {
-            content += feature.get('v_age') + 'に空き有り <a href="http://www.tsukuba-kosodate.jp/joyb9fd5a-261/?action=common_download_main&upload_id=3666" target="_blank">【詳細】</a>';
+            content += feature.get('v_age') + 'に空き有り <a href="http://www.tsukuba-kosodate.jp/joyb9fd5a-261/?action=common_download_main&upload_id=3868" target="_blank">【詳細】</a>';
 　　　　}
         content += '</td>';
         content += '</tr>';
@@ -422,7 +422,7 @@ Papamamap.prototype.getPopupContent = function(feature)
     if (feature.get('種別') == "認可保育所") {
         content += '<tr>';
         content += '<th>情報更新</th>';
-        content += '<td>2017/6/1</td>';
+        content += '<td>2017/8/2</td>';
         content += '</tr>';
     }
     if (feature.get('開始年齢') !== null && feature.get('終了年齢') !== null) {
